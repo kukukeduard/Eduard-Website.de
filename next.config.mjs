b44283051f764da4a1e92170c90ac5cf83548054
kukukeduard <kukukeduard@users.noreply.github.com>
2026-09-13 21:32:49 +0200

    Remove distDir completely for standard Next.js build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.178.23'],
-  distDir: process.env.NODE_ENV === 'development' ? '.next_dev' : '.next',
 };
 
 export default nextConfig;
